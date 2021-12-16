9c216e01dfbfb29ab66722aa1ab7c51ae7344939
saravananr <Saravanan.Ramalingam@data-axle.com>
2021-12-16 13:20:18 -0500

    `test`

--- phase1.py
+++ phase1.py
@@ -1,10 +1,24 @@
+import argparse
+import logging
 import sys
-import pyspark.sql.functions as f
-from pyspark import SQLContext
-from pyspark.context import SparkContext
+
 from pyspark.sql import SparkSession
 
-print(sys.path)
+ll_argv = sys.argv[1:]
+parser = argparse.ArgumentParser()
+parser.add_argument('-l', "--loglevel", help="Log Level")
+parser.add_argument('-c', "--CheckLevel", help="Check Level")
+args = parser.parse_args()
+if args.loglevel is None or args.loglevel == "INFO":
+    loglevel = logging.INFO
+elif args.loglevel == "DEBUG":
+    loglevel = logging.DEBUG
+elif args.loglevel == "ERROR":
+    loglevel = logging.ERROR
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s: %(message)s')
+logger.info("Welcome")
 # sc  = SparkContext()
 # sqlContext = SQLContext(sc)
 spark = SparkSession.builder.getOrCreate()
