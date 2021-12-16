5f689e7edf08b2944d73697ec4e604950d4d03a3
saravananr <Saravanan.Ramalingam@data-axle.com>
2021-12-16 12:23:39 -0500

    test

--- phase1.py
+++ phase1.py
@@ -1,3 +1,11 @@
 import sys
+import pyspark.sql.functions as f
+from pyspark import SQLContext
+from pyspark.context import SparkContext
+from pyspark.sql import SparkSession
 
 print(sys.path)
+# sc  = SparkContext()
+# sqlContext = SQLContext(sc)
+spark = SparkSession.builder.getOrCreate()
+spark.sql("select 'HelloWorld'")
