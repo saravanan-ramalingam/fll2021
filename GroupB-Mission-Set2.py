78a5d9a74134e7d071c0c637ad6b0cfe22b4b296
saravananr <Saravanan.Ramalingam@data-axle.com>
2021-12-16 12:44:31 -0500

    `test`

--- GroupB-Mission-Set2.py
+++ GroupB-Mission-Set2.py
@@ -1,13 +1,13 @@
 from spike import PrimeHub, MotorPair
 
 hub = PrimeHub()
-tire = MotorPair('E', 'F')
+tire = MotorPair("E", "F")
 
 # Home Delivery Code
 
-tire = MotorPair('E', 'F')
+tire = MotorPair("E", "F")
 
-tire.move_tank(-10, 'in', left_speed=42, right_speed=40)
-tire.move_tank(-30, 'in', left_speed=40, right_speed=41)
-tire.move_tank(-16, 'in', left_speed=40, right_speed=46)
-tire.move_tank(-4, 'in', left_speed=70, right_speed=38)
+tire.move_tank(-10, "in", left_speed=42, right_speed=40)
+tire.move_tank(-30, "in", left_speed=40, right_speed=41)
+tire.move_tank(-16, "in", left_speed=40, right_speed=46)
+tire.move_tank(-4, "in", left_speed=70, right_speed=38)
