b8d3f226d5297ff2afdc858ec927d91540c072d8
saravananr <Saravanan.Ramalingam@data-axle.com>
2021-12-16 12:36:02 -0500

    `test`

--- GroupB-Mission-Set1.py
+++ GroupB-Mission-Set1.py
@@ -1,7 +1,4 @@
-from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, \
-    DistanceSensor, Motor, MotorPair
-from spike.control import wait_for_seconds, wait_until, Timer
-from math import *
+from spike import PrimeHub, Motor, MotorPair
 
 hub = PrimeHub()
 tire = MotorPair('E', 'F')
